fa44e8c312557e0569f6234c6e9101da30c891e5
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-01-26 22:15:06 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # ↑↑上の記述は削除しましょう↑↑
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
+
+  # https://master.tech-camp.in/v2/curriculums/4841
+  # https://master.tech-camp.in/v2/curriculums/4840
 end
