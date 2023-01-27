d16f91a7e5169b7d25b8bb24b694310cadf3275e
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-01-28 00:16:26 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,6 +3,28 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+  def create
+    # binding.pry
+
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
+
+
+
+
 end
 
 # https://master.tech-camp.in/v2/curriculums/4849
+# https://master.tech-camp.in/v2/curriculums/4711
