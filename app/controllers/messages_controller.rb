a53cef5e0401e8a87d06efeb80cfee8f91125acb
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-01-29 16:07:44 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,8 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])
     # https://master.tech-camp.in/v2/curriculums/4860
+    @messages = @room.messages.includes(:user)
+    # https://master.tech-camp.in/v2/curriculums/4863
   end
 
   def create
@@ -12,6 +14,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
     # https://master.tech-camp.in/v2/curriculums/4862
