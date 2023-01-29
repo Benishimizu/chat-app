acd7cbb7826d2aafa2b630d97c5213f09bebc03e
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-01-29 21:42:32 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   
+  has_one_attached :image
   validates :content, presence: true
   # https://master.tech-camp.in/v2/curriculums/4866
 end
